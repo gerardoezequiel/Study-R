bef1c36d1a5185fd3d2ca2d78406fd1d0ef9bff3
Gerardo Ezequiel <geraezemc@gmail.com>
2022-11-16 08:23:12 +0000

    test mtcars

---
+++ main.R
@@ -0,0 +1,17 @@
+print("hello world")
+
+library(ggplot2)
+
+#load mtcars dataset
+data(mtcars)
+
+#plot factor variable
+ggplot(mtcars, aes(factor(cyl))) + geom_bar()
+ggplot(mtcars, aes(x = wt, y = mpg)) + geom_point()
+
+#plot histogram bin width
+ggplot(mtcars, aes(x = wt)) + geom_histogram(binwidth = 1)
+
+#plot histogram bin number
+ggplot(mtcars, aes(x = wt)) + geom_histogram(bins = 10)
+
